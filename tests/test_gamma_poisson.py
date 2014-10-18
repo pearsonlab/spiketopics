8cce076d8d5fe1abeef1b2b31fa88ce080c574ea
John Pearson <jmxpearson@gmail.com>
2014-10-17 23:31:48 -0400

    refactor to put setup code in smaller functions

--- tests/test_gamma_poisson.py
+++ tests/test_gamma_poisson.py
@@ -4,63 +4,97 @@ Tests for Gamma-Poisson model.
 import numpy as np
 import scipy.stats as stats
 
-class Forwards_Backwards_Integration:
-    def setup_class():
+class Test_Forwards_Backwards_Integration:
+
+    @classmethod
+    def _setup_constants(self):
+        """
+        Set variables that determine the problem size.
+        """
+        self.U = 100  # units
+        self.T = 5000  # time points
+        self.K = 5  # categories
+        self.dt = 1 / 30  # time length of observation (in s)
+
+    @classmethod
+    def _make_transition_probs(self):
+        """
+        Make a vector of transition probabilities for each category:
+        2 rows x K columns
+        Row 0 contains 0 -> 1 transition probability
+        Row 1 contains 1 -> transition probability
+        """ 
+        # want 0 -> to be rare, 1 -> 1 to be a little less so
+        v0 = stats.beta.rvs(1, 200, size=self.K)
+        v1 = stats.beta.rvs(100, 1, size=self.K)
+        self.trans_probs = np.vstack([v0, v1]).T 
+
+    @classmethod
+    def _make_initial_states(self):
+        """
+        pick initial states for each category
+        """
+        z0_probs = stats.beta.rvs(50, 50, size=self.K)
+        self.z0 = stats.bernoulli.rvs(z0_probs)
+
+    @classmethod
+    def _make_chain(self):
+        """
+        Make a Markov chain by using the transition probabilities.
+        """
+
+        # make the chain by evolving each category through time
+        chain = np.empty((self.K, self.T), dtype='int')
+
+        # initialize
+        chain[:, 0] = self.z0
+
+        for t in xrange(1, self.T):
+            p_trans_to_1 = self.trans_probs[range(self.K), chain[:, t - 1]]
+            chain[:, t] = stats.bernoulli.rvs(p_trans_to_1)
+            
+        # add a baseline that's always turned on
+        chain[0, :] = 1
+
+        self.chain = chain
+
+    @classmethod
+    def _make_firing_rates(self):
+        """
+        Make matrix of firing rates (categories x units)
+        """        
+
+        aa = 3, 1
+        bb = 2, 1
+        lam = stats.gamma.rvs(a=aa[1], scale=bb[1], size=(self.K, self.U))
+
+        # baselines should follow a different distribution
+        lam[0, :] = stats.gamma.rvs(a=aa[0], scale=bb[0], size=self.U)   
+
+        # calculate rate for each time
+        fr = np.exp(self.chain.T.dot(np.log(lam))) * self.dt
+        self.fr = fr + 1e-5  # in case we get exactly 0
+
+    @classmethod
+    def setup_class(self):
         """
         Make some sample Markov chain data to run inference on.
         """
 
         np.random.seed(12345)
 
-        U = 100  # units
-        T = 5000  # time points/frames
-        Kdata = 5  # categories
-        dt = 1 / 30  # frames per second
+        self._setup_constants()
 
-        # transition probabilities
-        v0_a = 1
-        v0_b = 200
-        v1_a = 100
-        v1_b = 1
+        self._make_transition_probs()
 
-        # make vector of transition probabilities
-        # each row is a category, each column the transition
-        # probability from that state to state 1
-        # (this is easy to do without matrices since we have
-        # only two states)
-        v0 = stats.beta.rvs(v0_a, v0_b, size=Kdata)
-        v1 = stats.beta.rvs(v1_a, v1_b, size=Kdata)
-        trans_probs = np.vstack([v0, v1]).T 
+        self._make_initial_states()
 
-        # pick initial states for each category
-        z0_probs = stats.beta.rvs(50, 50, size=Kdata)
-        z0 = stats.bernoulli.rvs(z0_probs)
+        self._make_chain()
 
-        # make the chain by evolving each category through time
-        chain = np.empty((Kdata, T), dtype='int')
-
-        # initialize
-        chain[:, 0] = z0
-
-        for t in xrange(1, T):
-            p_trans_to_1 = trans_probs[range(Kdata), chain[:, t - 1]]
-            chain[:, t] = stats.bernoulli.rvs(p_trans_to_1)
-            
-        # include a baseline regressor that is always turned on
-        chain[0, :] = 1
-
-        # set up firing rates for each category, unit
-        aa = 3, 1
-        bb = 2, 1
-        lam = stats.gamma.rvs(a=aa[1], scale=bb[1], size=(Kdata, U))
-        lam[0, :] = stats.gamma.rvs(a=aa[0], scale=bb[0], size=U)  # baselines 
-
-        # calculate rate for each time
-        fr = np.exp(chain.T.dot(np.log(lam))) * dt
-        fr = fr + 1e-5  # in case we get exactly 0
+        self._make_firing_rates()
 
         # draw from Poisson
-        N = stats.poisson.rvs(fr)
+        self.N = stats.poisson.rvs(self.fr)
 
-    def test_stuff():
-        pass
+    def test_stuff(self):
+        assert(0 == 0)
