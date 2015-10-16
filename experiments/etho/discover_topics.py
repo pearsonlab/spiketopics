16e6623a61a03bc9677ecc50af09a76e171ea6b4
John Pearson <jmxpearson@gmail.com>
2015-10-16 16:04:46 -0400

    reverted discover_topics to version on master
    - necessary because modifications to run segmentation code are now in
      discover_topics_segment

--- experiments/etho/discover_topics.py
+++ experiments/etho/discover_topics.py
@@ -4,13 +4,13 @@ Fit Gamma-Poisson topic model to movie/ethogram dataset.
 from __future__ import division
 import numpy as np
 import pandas as pd
-import spiketopics.gamma_segment_model as gp
+import spiketopics.gamma_model as gp
 from spiketopics.helpers import jitter_inits
 import argparse
 import cPickle as pickle
 
 if __name__ == '__main__':
-
+    
     parser = argparse.ArgumentParser(description="Run latent topic discovery using a Gamma-Poisson model")
     parser.add_argument("input", help="name of input file")
     parser.add_argument("-s", "--seed", help="random number seed",
@@ -27,20 +27,21 @@ if __name__ == '__main__':
     df = pd.read_csv(datfile)
 
     # ####### for testing only ################
-    print "Subsetting for testing..."
-    df = df.query('time <= 1e4')
-    # and renumber units consecutively (starting at 0)
-    df['unit'] = np.unique(df['unit'], return_inverse=True)[1]
+    # print "Subsetting for testing..."
+    # df = df.query('time <= 1e5')
+    # # and renumber units consecutively (starting at 0)
+    # df['unit'] = np.unique(df['unit'], return_inverse=True)[1]
     # ####### for testing only ################
 
-    # set up params
+    # set up params 
     print "Calculating parameters..."
     dt = 1. / 30  # duration of movie frame
     M = df.shape[0]
     T = df['time'].drop_duplicates().shape[0]
     U = df['unit'].drop_duplicates().shape[0]
     R = df.shape[1] - len(['time', 'unit', 'count'])
-    K = 5
+    K = 10
+    D = 500  # maximum semi-Markov duration
     Mz = 2  # number of levels of each latent state
 
     #################### priors and initial values
@@ -55,16 +56,16 @@ if __name__ == '__main__':
 
 
     baseline_dict = ({
-                'prior_shape_shape': bl_shape_shape,
-                'prior_shape_rate': bl_shape_rate,
-                'prior_mean_shape': bl_mean_shape,
+                'prior_shape_shape': bl_shape_shape, 
+                'prior_shape_rate': bl_shape_rate, 
+                'prior_mean_shape': bl_mean_shape, 
                 'prior_mean_rate': bl_mean_rate,
-                'post_shape_shape': bl_shape_shape,
-                'post_shape_rate': bl_shape_rate,
-                'post_mean_shape': bl_mean_shape,
+                'post_shape_shape': bl_shape_shape, 
+                'post_shape_rate': bl_shape_rate, 
+                'post_mean_shape': bl_mean_shape, 
                 'post_mean_rate': bl_mean_rate,
-                'post_child_shape': np.ones((U,)),
-                'post_child_rate': np.ones((U,))
+                'post_child_shape': np.ones((U,)), 
+                'post_child_rate': np.ones((U,)) 
                 })
 
     ############ firing rate latents ####################
@@ -74,15 +75,15 @@ if __name__ == '__main__':
     fr_mean_rate = 1000 * np.ones((K,))
 
     fr_latent_dict = ({
-                'prior_shape_shape': fr_shape_shape,
-                'prior_shape_rate': fr_shape_rate,
-                'prior_mean_shape': fr_mean_shape,
+                'prior_shape_shape': fr_shape_shape, 
+                'prior_shape_rate': fr_shape_rate, 
+                'prior_mean_shape': fr_mean_shape, 
                 'prior_mean_rate': fr_mean_rate,
-                'post_shape_shape': fr_shape_shape,
-                'post_shape_rate': fr_shape_rate,
-                'post_mean_shape': fr_mean_shape,
+                'post_shape_shape': fr_shape_shape, 
+                'post_shape_rate': fr_shape_rate, 
+                'post_mean_shape': fr_mean_shape, 
                 'post_mean_rate': fr_mean_rate,
-                'post_child_shape': np.ones((U, K)),
+                'post_child_shape': np.ones((U, K)), 
                 'post_child_rate': np.ones((U, K))
                 })
 
@@ -92,26 +93,26 @@ if __name__ == '__main__':
     # A_on = 1.
     # Avec = np.r_[A_off, A_on].reshape(2, 1, 1)
     # A_prior = np.tile(Avec, (1, 2, K))
-    # A_cat = (10 * np.eye(2) + 1).reshape(2, 2, 1)
-    # A_prior = np.tile(A_cat, (1, 1, K))
+    A_cat = (10 * np.eye(2) + 1).reshape(2, 2, 1)
+    A_prior = np.tile(A_cat, (1, 1, K))
 
-    # ###### pi ###############
-    # pi_off = 15.
-    # pi_on = 1.
-    # pi_prior = np.tile(np.r_[pi_off, pi_on].reshape(2, 1), (1, K))
+    ###### pi ###############
+    pi_off = 15.
+    pi_on = 1.
+    pi_prior = np.tile(np.r_[pi_off, pi_on].reshape(2, 1), (1, K))
 
-    # ###### p(d) #############
-    # d_hypers = (2.5, 4., 2., 40.)
-    # d_pars = ({'d_prior_mean': d_hypers[0] * np.ones((Mz, K)),
-    #           'd_prior_scaling': d_hypers[1] * np.ones((Mz, K)),
-    #           'd_prior_shape': d_hypers[2] * np.ones((Mz, K)),
-    #           'd_prior_rate': d_hypers[3] * np.ones((Mz, K))})
-    # d_inits = (3., 1., 1., 1.)
+    ###### p(d) #############
+    d_hypers = (2.5, 4., 2., 40.)
+    d_pars = ({'d_prior_mean': d_hypers[0] * np.ones((Mz, K)), 
+              'd_prior_scaling': d_hypers[1] * np.ones((Mz, K)),
+              'd_prior_shape': d_hypers[2] * np.ones((Mz, K)),
+              'd_prior_rate': d_hypers[3] * np.ones((Mz, K))})
+    d_inits = (3., 1., 1., 1.)
 
-    # d_post_pars = ({'d_post_mean': d_inits[0] * np.ones((Mz, K)),
-    #                 'd_post_scaling': d_inits[1] * np.ones((Mz, K)),
-    #                 'd_post_shape': d_inits[2] * np.ones((Mz, K)),
-    #                 'd_post_rate': d_inits[3] * np.ones((Mz, K))})
+    d_post_pars = ({'d_post_mean': d_inits[0] * np.ones((Mz, K)), 
+                    'd_post_scaling': d_inits[1] * np.ones((Mz, K)),
+                    'd_post_shape': d_inits[2] * np.ones((Mz, K)),
+                    'd_post_rate': d_inits[3] * np.ones((Mz, K))})
 
 
     # E[z]
@@ -122,29 +123,16 @@ if __name__ == '__main__':
     z_prior = np.dstack([1 - xi_mat, xi_mat]).transpose((2, 0, 1))
 
     # E[zz]
-    # Xi_mat = np.random.rand(2, 2, T - 1, K)
-    # Xi_mat = Xi_mat.astype('float')
+    Xi_mat = np.random.rand(2, 2, T - 1, K)
+    Xi_mat = Xi_mat.astype('float')
 
-    # latent_dict = ({'A_prior': A_prior, 'pi_prior': pi_prior,
-    #                 'A_post': A_prior, 'pi_post': pi_prior,
-    #                 'z_init': z_prior, 'zz_init': Xi_mat,
-    #                 'logZ_init': np.zeros((K,))
-    #                 })
-
-    # prior params
-    theta = 0.5  # prior on E[z] for each segment
-    beta = 2.  # desired value for changepoint penalty
-    alpha = beta + np.log(1 - theta)  # penalty on number of changepoints
-
-    # for parallel processing, we need a list of tuples defining start and
-    # end times of chunks
-    movie_df = pd.read_csv('data/movie_start_end_times.csv')
-    chunklist = zip(movie_df['start'], movie_df['end'])
-
-    latent_dict = ({'z_init': z_prior, 'theta': theta, 'alpha': alpha,
-        'chunklist': chunklist})
-    # latent_dict.update(d_pars)
-    # latent_dict.update(d_post_pars)
+    latent_dict = ({'A_prior': A_prior, 'pi_prior': pi_prior,
+                    'A_post': A_prior, 'pi_post': pi_prior, 
+                    'z_init': z_prior, 'zz_init': Xi_mat, 
+                    'logZ_init': np.zeros((K,))
+                    })
+    latent_dict.update(d_pars)
+    latent_dict.update(d_post_pars)
     ############ regression coefficients ####################
     ups_shape = 11.
     ups_rate = 10.
@@ -166,9 +154,9 @@ if __name__ == '__main__':
     ############ overdispersion ####################
     od_shape = 6.
     od_rate = 5.
-    od_dict = ({'prior_shape': od_shape * np.ones((M,)),
-                'prior_rate': od_rate * np.ones((M,)),
-                'post_shape': np.ones((M,)),
+    od_dict = ({'prior_shape': od_shape * np.ones((M,)), 
+                'prior_rate': od_rate * np.ones((M,)), 
+                'post_shape': np.ones((M,)), 
                 'post_rate': np.ones((M,))
                 })
 
@@ -178,14 +166,14 @@ if __name__ == '__main__':
     Lvals = []
     for idx in xrange(numstarts):
         # set up model object
-        gpm = gp.GammaModel(df, K)
+        gpm = gp.GammaModel(df, K, D)
         gpm.initialize_baseline(**jitter_inits(baseline_dict, 0.25))
         gpm.initialize_fr_latents(**jitter_inits(fr_latent_dict, 0.25))
         gpm.initialize_latents(**jitter_inits(latent_dict, 0.25))
         gpm.initialize_fr_regressors(**jitter_inits(reg_dict, 0.25))
         gpm.initialize_overdispersion(**jitter_inits(od_dict, 0.25))
         gpm.finalize()
-
+        
         print "Start {} -----------------------".format(idx)
         gpm.do_inference(tol=1e-4, verbosity=2)
         print "Final L = {}".format(gpm.L())
@@ -207,11 +195,20 @@ if __name__ == '__main__':
         for a in to_delete:
             delattr(node, a)
 
+    # for semi-Markov, more stuff to delete
+    try:
+        dnode = gpm.nodes['HMM'].nodes['d']
+        to_delete = ['logpd', 'calc_ess', 'update']
+        for fn in to_delete:
+            delattr(dnode, fn)
+    except:
+        pass
+
     print "Writing to disk..."
     outfile = 'data/fitted_model_object.pkl'
     pickle.dump(gpm, open(outfile, 'wb'))
 
     features_outfile = 'data/features.csv'
-    xi = gpm.nodes['segmodel'].nodes['z'].z[1]
+    xi = gpm.nodes['HMM'].nodes['z'].z[1]
     zdf = pd.DataFrame(xi, columns = ['Z' + str(k) for k in xrange(K)])
     zdf.to_csv(features_outfile, index=False)
