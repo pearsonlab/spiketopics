49e592d7210109aa338cef94feeac7d3e393fbe8
John Pearson <jmxpearson@gmail.com>
2016-04-01 16:14:53 -0400

    more code in lognormal plus basic tests

--- lognormal.py
+++ lognormal.py
@@ -4,13 +4,14 @@ and gradient ascent.
 """
 from __future__ import division
 from __future__ import print_function
+from scipy.special import digamma, gammaln
 import autograd.numpy as np
 import autograd.numpy.random as npr
 from fbi import fb_infer
 
 def L(N, eta_mean, eta_cov):
     elbo = log_observed_spikes(N, eta_mean, eta_cov)
-    elbo += mvnormal_entropy(eta_mean, eta_cov)
+    elbo += normal_entropy(eta_mean, eta_cov)
     return elbo
 
 def log_observed_spikes(N, mu, Sig):
@@ -45,8 +46,9 @@ def expected_log_mvnormal(m, S, mu, Sig):
     Vector index of normal is assumed to be *last* index
     """
     Lam = np.linalg.inv(S)
-    x = mu - mu
-    out = -0.5 * (Sig * Lam + np.einsum('...ij, ...i, ...j', Lam, x, x))
+    x = m - mu
+    out = -0.5 * np.einsum('...ij, ...ij', Sig, Lam)
+    out += -0.5 * np.einsum('...ij, ...i, ...j', Lam, x, x)
     return np.sum(out)
 
 def normal_entropy(mu, sig):
@@ -54,7 +56,6 @@ def normal_entropy(mu, sig):
     Entropy of multivariate normal.
     sig is **variance**
     """
-    T, U = mu.shape
     out = mu.size * 0.5 * (np.log(np.pi) + 1)
 
     out += 0.5 * np.sum(np.log(sig))
@@ -85,12 +86,12 @@ def hmm_entropy(log_psi, log_A, log_pi, xi, Xi, logZ):
     three arguments
     """
     emission_piece = np.sum(xi * log_psi)
-    initial_piece = xi[:, 0].dot(log_pi)
+    initial_piece = xi[0].dot(log_pi)
     transition_piece = np.sum(Xi * log_A)
     logq = emission_piece + initial_piece + transition_piece
     return -logq + logZ
 
-def expected_log_state_sequence(xi, Xi, EA, Epi):
+def expected_log_state_sequence(xi, Xi, Elog_A, Elog_pi):
     """
     Expected log of Markov state sequence
     xi: (T, M) -- q(z_t)  (posterior marginal)
@@ -98,6 +99,37 @@ def expected_log_state_sequence(xi, Xi, EA, Epi):
     Elog_A: (M, M) -- Expected value of log transition matrix
     Elog_pi: (M,) -- Expected value of log initial state probability
     """
-    initial_piece = xi[:, 0].dot(Elog_pi)
+    initial_piece = xi[0].dot(Elog_pi)
     transition_piece = np.sum(Xi * Elog_A)
     return initial_piece + transition_piece
+
+def _logB(alpha):
+    """
+    logB = sum_i log Gamma(alpha_i) - log Gamma(sum_i alpha_i)
+    """
+    return np.sum(gammaln(alpha), axis=-1) - gammaln(np.sum(alpha, axis=-1))
+
+def dirichlet_entropy(alpha):
+    """
+    Entropy of collection of Dirichlet distributions.
+    *Last* index of alpha is the index that sums to 1.
+    """
+    alpha0 = np.sum(alpha, axis=-1)
+    H = _logB(alpha)
+    H += (alpha0 - alpha.shape[-1]) * digamma(alpha0)
+    H += -np.sum((alpha - 1) * digamma(alpha), axis=-1)
+
+    return np.sum(H)
+
+def expected_log_dirichlet(a, alpha):
+    """
+    E[log p(x)] where
+    p(x) = Dirichlet(a)
+    q(x) = Normal(alpha)
+    *Last* index of a/alpha is the index that sums to 1.
+    """
+    Elog_x = digamma(alpha) - digamma(np.sum(alpha, axis=-1, keepdims=True))
+    elp = np.sum((a - 1) * Elog_x, axis=-1)
+    elp += -_logB(a)
+
+    return np.sum(elp)
